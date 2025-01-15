5d883bcb720da6e7d287b98872c13b7cb941ee41
navin1994 <navin.yele@gmail.com>
2025-01-15 14:18:28 +0530

    Updated code with use of signal

--- src/app/config-builder/form-layout/form-layout.component.ts
+++ src/app/config-builder/form-layout/form-layout.component.ts
@@ -1,39 +1,47 @@
-import { Component, Input, input, OnInit } from '@angular/core';
+import { Component, computed, Input, OnInit, signal } from '@angular/core';
 import { UntypedFormGroup } from '@angular/forms';
 import { MatCardModule } from "@angular/material/card";
 import { FieldSelectorComponent } from '../field-selector/field-selector.component';
 import { AccessControls, AttributeType, ConditionGroup, ElementLayoutData, FormConfig } from 'src/app/models/ui-form-config.interface';
-import { debounceTime} from 'rxjs/operators';
+import { debounceTime } from 'rxjs/operators';
 import { areObjectsEqual } from 'src/app/utility/utility';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-form-layout',
   standalone: true,
-  imports: [MatCardModule, FieldSelectorComponent],
+  imports: [MatCardModule, FieldSelectorComponent, AsyncPipe],
   templateUrl: './form-layout.component.html',
   styleUrl: './form-layout.component.scss'
 })
-export class FormLayoutComponent implements OnInit{
-  config = input.required<FormConfig>();
-  @Input() formGroup: UntypedFormGroup = new UntypedFormGroup({});
-  visibleLayers: ElementLayoutData[][];
-  previousFormValue: Record<string, any>;
+export class FormLayoutComponent implements OnInit {
+  @Input({ required: true }) formGroup: UntypedFormGroup;
+  @Input({ required: true }) config!: FormConfig;  
+  
+  // Signals for visibility layers and previous form value
+  visibleLayers = signal<ElementLayoutData[][]>([]);
+  previousFormValue = signal<Record<string, any>>({});
 
   ngOnInit(): void {
-    this.visibleLayers = this.config().ui.elementsLayout;
+    this.visibleLayers.set(this.config.ui.elementsLayout);
     this.formGroup.valueChanges.pipe(debounceTime(500)).subscribe((formData) => {
-      if (areObjectsEqual(formData, this.previousFormValue)) return;
-      this.previousFormValue = formData;
-      let layers: ElementLayoutData[][] = this.config().ui.elementsLayout;
-      const { attributes } = this.config().ui.references;
-      for (const id in attributes) {
-        const result = !this.computeAccessControl('visibility', attributes, id, formData);
-        if (result) {
-          layers = this.removeAttributeAndCleanup(layers, id);
-        }
-      }
-      this.visibleLayers = layers;
+      if (areObjectsEqual(formData, this.previousFormValue())) return;
+      this.previousFormValue.set(formData);
+      this.updateVisibleLayers(formData);
     });
+  }
+
+  private updateVisibleLayers(formData: Record<string, any>): void {
+    let layers = this.config.ui.elementsLayout;
+    const { attributes } = this.config.ui.references;
+
+    for (const id in attributes) {
+      const isVisible = this.computeAccessControl('visibility', attributes, id, formData);
+      if (!isVisible) {
+        layers = this.removeAttributeAndCleanup(layers, id);
+      }
+    }
+    this.visibleLayers.set(layers);
   }
 
   removeAttributeAndCleanup(arr: ElementLayoutData[][], attribute: string) {
@@ -56,36 +64,37 @@ export class FormLayoutComponent implements OnInit{
   }
 
   computeAccessControl(checkType: string, attributes: any, id: string, data: Record<string, any>): boolean {
-    const isExist = checkType in attributes[id]
+    const isExist = checkType in attributes[id];
     if (!isExist) return true;
-      const { matchAllGroup, matchConditionsGroup, conditionGroups } = attributes[id].visibility as AccessControls;
-      const mainFunc = matchAllGroup ? 'every' : 'some';
-      const groupFunc = matchConditionsGroup ? 'every' : 'some';
-      return conditionGroups?.[mainFunc]((groupArray) => {
-        return groupArray?.[groupFunc]((group: ConditionGroup)=> {
-          if (group.attributeType === ('form-attribute' as AttributeType) && group?.sourceAttribute) {
-            const currentValue = data[group.sourceAttribute]?.toString() ?? 'false';
-            switch (group.condition) {
-              case 'equal':
-                return currentValue == group.conditionValue;
-              case 'not-equal':
-                return currentValue != group.conditionValue;
-              case 'regex':
-                const regex = new RegExp(group.conditionValue);
-                return regex.test(currentValue);
-              case 'contains':
-                return currentValue?.includes(group.conditionValue);
-              case 'start-with':
-                return currentValue?.startsWith(group.conditionValue);
-              case 'greater-than':
-                return Number(data[group.sourceAttribute]) > Number(group.conditionValue);
-              case 'less-than':
-                return Number(data[group.sourceAttribute]) < Number(group.conditionValue);
-            }
-          } else {
-            return true;
+
+    const { matchAllGroup, matchConditionsGroup, conditionGroups } = attributes[id].visibility as AccessControls;
+    const mainFunc = matchAllGroup ? 'every' : 'some';
+    const groupFunc = matchConditionsGroup ? 'every' : 'some';
+
+    return conditionGroups?.[mainFunc]((groupArray) => {
+      return groupArray?.[groupFunc]((group: ConditionGroup) => {
+        if (group.attributeType === ('form-attribute' as AttributeType) && group?.sourceAttribute) {
+          const currentValue = data[group.sourceAttribute]?.toString() ?? 'false';
+          switch (group.condition) {
+            case 'equal':
+              return currentValue === group.conditionValue;
+            case 'not-equal':
+              return currentValue !== group.conditionValue;
+            case 'regex':
+              const regex = new RegExp(group.conditionValue);
+              return regex.test(currentValue);
+            case 'contains':
+              return currentValue?.includes(group.conditionValue);
+            case 'start-with':
+              return currentValue?.startsWith(group.conditionValue);
+            case 'greater-than':
+              return Number(data[group.sourceAttribute]) > Number(group.conditionValue);
+            case 'less-than':
+              return Number(data[group.sourceAttribute]) < Number(group.conditionValue);
           }
-        });
+        }
+        return true;
       });
+    });
   }
 }
